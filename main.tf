f638670bf4dd97b4da9823ca0ab167434fe1b38e
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-07-03 18:44:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,28 @@ terraform {
   }
 }
 
-provider "google" {
-  credentials = file("<NAME>.json")
-
-  project = "<PROJECT_ID>"
+  project = "hc-298fe22b114c4f4f8961e031e5c"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
