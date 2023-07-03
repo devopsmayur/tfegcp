5ba76cb15baafd246f91ae8feb6afe2cb9ab5f66
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-07-03 18:56:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,16 +34,3 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 }
-
-check "check_vm_status" {
-  data "google_compute_instance" "vm_instance" {
-    name = google_compute_instance.vm_instance.name
-  }
-  assert {
-    condition = data.google_compute_instance.vm_instance.current_status == "RUNNING"
-    error_message = format("Provisioned VMs should be in a RUNNING status, instead the VM `%s` has status: %s",
-      data.google_compute_instance.vm_instance.name,
-      data.google_compute_instance.vm_instance.current_status
-    )
-  }
-}
