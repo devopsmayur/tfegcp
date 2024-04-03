6282525e9ac2e6095f1b86971050e3c1464cfe50
Mayur <99334268+devopsmayur@users.noreply.github.com>
2024-04-03 19:58:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,6 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "f1-micro"
